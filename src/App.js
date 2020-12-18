63f146a19a2a3603cea27c82f0cf0e09296a2e04
Andrew Sloan <sloancodes@gmail.com>
2020-12-18 14:46:00 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Container, } from "semantic-ui-react";
+import { Switch, Route, } from "react-router-dom";
+import UserProfile from "./components/UserProfile";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  
+    <Container>
+      <Switch>
+        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route
+          exact
+          path="/account/profile"
+          render={() => <UserProfile />}
+        />
+      </Switch>
+    </Container>
+  </>
   );
 }
 
