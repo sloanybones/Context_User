d973d6a324d522ed1f0967cf61c6f60d274f4b96
Andrew Sloan <sloancodes@gmail.com>
2020-12-21 10:24:04 -0700

    form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,5 +1,38 @@
-const UserForm = () => {
-  return(
-    "hello"
-  )
-}
+import React, { useState, useContext } from "react";
+import { Form } from "semantic-ui-react";
+import { UserConsumer, UserContext } from "../providers/UserProvider";
+
+const AccountForm = (props) => {
+  const [userName, setUsername] = useState(defaultUsername);
+  const [email, setEmail] = useState(
+    defaultMembershipLevel
+  );
+  
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Input
+        label="New Username"
+        type="text"
+        name="username"
+        value={username}
+        onChange={(e, { value }) => {
+          setUsername(value);
+        }}
+      />
+      <Form.Select
+        label="Email"
+        name="email"
+        value={email}
+        onChange={(e, { value }) => {
+          setEmail);
+       
+      />
+      <Form.Button color="blue">Save</Form.Button>
+    </Form>
+  );
+};
+        }
+
+
+export default UserForm;
